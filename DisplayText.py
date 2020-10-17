f895fc9e03683082812c708ae469d9d126b039fb
jaweria332 <hiyamughal03@gmail.com>
2020-10-17 08:05:16 -0700

    Addin files
    Code simply displays the text enter by user in a separate entry

--- DisplayText.py
+++ DisplayText.py
@@ -16,9 +16,10 @@ root.config(bg="#262626")
 #Declaring whether windows in resizable or not
 root.resizable(False, False)
 
+
 #Definig  a function to get the text
 def get_label_text():
-    res_entry.config(text=str(txt_name.get())
+    res_entry.config(text=str(txt_name.get()))
 
 #Title
 lbl_res_title=Label(root, text="LETS DISPLAY USER TEXT", font=("Times New Roman", 20, "bold"), bg="orange", fg="brown", padx=20, pady=20).pack(fill=X)
@@ -30,14 +31,14 @@ txt_name = Entry(root, font=("Times New Roman", 14, "normal"), bg="white", fg="b
 txt_name.place(x=160, y=100)
 
 #Defining button
-btn = Button(root, text="Submit",font=("Times New Roman", 14, "normal"), bg="orange", fg="brown", padx=20, cursor="hand2", activebackground="lightgreen", activeforeground="green", command=get_label_text()).place(x=200,y=150)
+btn = Button(root, text="Submit",font=("Times New Roman", 14, "normal"), bg="orange", fg="brown", padx=20, cursor="hand2", activebackground="lightgreen", activeforeground="green", command=get_label_text).place(x=200,y=150)
 
 
 #Header
-lbl_res = Label(root, text="YOU ENTERED", font=("Times New Roman", 22, "bold"), bg="#262626", fg="white").place(x=140, y=250)
-res_entry=Entry(root, font=("Times New Roman", 18, "normal"), bg="black", fg="white")
+lbl_res = Label(root, text="YOU ENTERED", font=("Times New Roman", 22, "bold"), bg="#262626",fg="white").place(x=140, y=250)
+res_entry=Label(root, font=("Times New Roman", 18, "normal"), bg="black", fg="white")
 res_entry.place(x=140, y=300)
 
 
-#Indicate End of windows and to hold windows infinitely
+#Indicate End of windows and to hold windows infinitely  *.md; *.txt; *.rst; *.adoc; *.html
 root.mainloop()
