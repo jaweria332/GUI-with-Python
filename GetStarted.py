d277b7116677d6a918b4808f614a2d4057aa37aa
jaweria332 <hiyamughal03@gmail.com>
2020-10-17 05:25:16 -0700

    Beginning with GUI

---
+++ GetStarted.py
@@ -0,0 +1,20 @@
+#Import Tkinter, a library used for GUI in Python
+from tkinter import *
+
+#Declaring a root object
+root=Tk()
+
+#Define title of your window
+root.title("Gets Started with Python GUI")
+
+#Defining the windows size
+root.geometry("500x600+700+80")
+
+#Defining background color
+root.config(bg="#262626")
+
+#Declaring whether windows in resizable or not
+root.resizable(False, False)
+
+#Indicate End of windows and to hold windows infinitely
+root.mainloop()
